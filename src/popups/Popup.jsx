1ced632f986922f5db55499e450ddf1b80d2442a
MSzostaq <maciek.szostak55@gmail.com>
2021-03-01 18:35:20 +0100

    popup - small refactoring

--- src/popups/Popup.jsx
+++ src/popups/Popup.jsx
@@ -1,11 +1,11 @@
 import React, { useRef } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
-// import OutsideClickHandler from "react-outside-click-handler";
+import OutsideClickHandler from "react-outside-click-handler";
 import Icon from "components/Icon";
 
 const Overlay = styled.div`
-  background-color: rgba(0, 0, 0, 0.3);
+  background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -17,7 +17,9 @@ const Overlay = styled.div`
 const CloseButton = styled(Icon)`
   color: #bdbdbd;
   cursor: pointer;
-  position: fixed;
+  position: absolute;
+  top: 8px;
+  right: 8px;
   width: 48px;
   height: 48px;
 `;
@@ -25,7 +27,7 @@ const CloseButton = styled(Icon)`
 const Content = styled(motion.div)`
   background-color: #f8f8f8;
   border-radius: 12px;
-  box-shadow: 0 4px 16px rgba(0, 0, 0 0.2);
+  box-shadow: 0 4px 16px rgba(0, 0, 0 0.3);
   color: #000;
   position: relative;
   width: 900px;
